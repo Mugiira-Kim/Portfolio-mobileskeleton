eec8426e391ca47fde80bfd01a884173cf36d1f1
Felix Ouma <fatonoh@gmail.com>
2022-05-09 22:44:11 +0300

    Refactor mobile menu navigation drawer

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-const hambugerMenu = document.querySelector('.menu-icon');
 const mobileMenu = document.querySelector('.menu-wrapper .menu ul');
-const closeMenu = document.querySelector('.close-icon');
-const menuItems = document.querySelectorAll('.menu-item');
+const menuItems = document.querySelectorAll('.toggle-menu');
 
 const toggleMenu = () => {
-    mobileMenu.classList.toggle('mobile-menu');
-  };
+  mobileMenu.classList.toggle('mobile-menu');
+};
 
-hambugerMenu.addEventListener('click', toggleMenu);
-closeMenu.addEventListener('click', toggleMenu);
 menuItems.forEach((menuItem) => { menuItem.addEventListener('click', toggleMenu); });
