b366c368dc57a8995c2c2f478655dae7522f354d
Felix Ouma <fatonoh@gmail.com>
2022-05-12 16:52:47 +0300

    Add styling to form validation alert message

--- submit.js
+++ submit.js
@@ -3,11 +3,12 @@ const form = document.querySelector('form');
 const error = document.querySelector('.error');
 
 form.addEventListener('submit', (event) => {
-const emailInput = emailCont.value;
-if (emailInput.toLowerCase() !== emailInput) {
-error.textContent = 'Please insert email address in lowercase!';
-event.preventDefault(); 
-} else {
-form.submit();
-}
+  const emailInput = emailCont.value;
+  if (emailInput.toLowerCase() !== emailInput) {
+    error.textContent = 'Please insert email address in lowercase!';
+    error.classList.add('d-flex');
+    event.preventDefault();
+  } else {
+    form.submit();
+  }
 });
